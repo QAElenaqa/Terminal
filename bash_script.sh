f4a57e848f913467451884afc1dab26db4ccd90b
Elenaqa1 <elena.kulesh@intexsoft.by>
2022-06-02 11:59:09 +0300

    add bash script

---
+++ bash_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd group_30
+mkdir Q1 Q2 Q3
+cd Q1
+touch q1.txt q2.txt q3.txt q4.json q5.json
+mkdir Q4 Q5 Q6
+ls -la
+mv /home/INTEXSOFT/elena.kulesh/Desktop/Terminal/group_30/Q1/{q2.txt,q1.txt} /home/INTEXSOFT/elena.kulesh/Desktop/Terminal/group_30/Q2
+echo "Все готово!!!"
+
